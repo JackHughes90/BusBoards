1501a2fc7d97193c3efe1433cd91b7200970e3d4
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-11 15:57:09 +0100

    now produces usable data

--- BusBoard.js
+++ BusBoard.js
@@ -1,18 +1,40 @@
 const readline = require('readline-sync');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
-// Stop trying to make "fetch" happen.
 
-function getBusStopData(askForStopCode) {
+async function getBusStopData(askForStopCode) {
     console.log(askForStopCode);
     stopCode = readline.prompt();
     // apiRequest = "https://api.tfl.gov.uk/StopPoint/"+stopCode+"/Arrivals"
     apiRequest = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals"
     //console.log(stopCode);
-    arrayOfBus = fetch(apiRequest)
-    .then(response => response.json());
-    // .then(body => console.log(body));
 
-    console.log(arrayOfBus);
+    
+    const response = await fetch(apiRequest);
+    const data = await response.json();
+
+    var nextBus = {};
+    
+    for (i = 0; i < 5; i++) {
+        nextBus['Bus name'][i]=data[i].lineName;
+        nextBus['Wait Time'][i]=data[i].timeToStation;
+        nextBus['Destination'][i]=data[i].destionationName;
+        nextBus['Route'][i]=data[i].towards;
+    }
+    
+    console.log(nextBus);
+    
+
+
+    // arrayOfBus[0].lineName
+    
+    /*
+    arrayOfBus = fetch(apiRequest)
+    .then(response => response.json())
+    .then(body => console.log(body));
+
+    //console.log(arrayOfBus);
+
+    */
 }
 
 getBusStopData("Hello, bus stop code would you like to inspect?");
