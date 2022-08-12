d63a7e5beba444f96337584002b55bb6ca0cf06f
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-08-12 11:06:40 +0100

    Added check with postcode

--- BusBoard.js
+++ BusBoard.js
@@ -4,8 +4,8 @@ const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch
 async function getBusStopData(requestStopCode) {
     console.log(requestStopCode);
     stopCode = readline.prompt();
-    // apiRequest = "https://api.tfl.gov.uk/StopPoint/"+stopCode+"/Arrivals"
-    apiRequest = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals"
+    apiRequest = "https://api.tfl.gov.uk/StopPoint/"+stopCode+"/Arrivals"
+    //apiRequest = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals"
 
     // fetches API and creates "data" array to hold them in.
     const busStopResponse = await fetch(apiRequest);
@@ -29,14 +29,30 @@ async function getBusStopData(requestStopCode) {
     console.log(nextBus);
 }
 
-function findNearestStops(requestPostCode) {
+async function findNearestStops(requestPostCode) {
     console.log(requestPostCode);
     postCode = readline.prompt();
-    // pcApiRequest = "api.postcodes.io/postcodes/"+postCode
-    pcApiRequest = "api.postcodes.io/postcodes/N129HB"
+    pcApiRequest = "https://api.postcodes.io/postcodes/"+postCode
+    //pcApiRequest = "https://api.postcodes.io/postcodes/N129HB"
+    const postCodeResponse = await fetch(pcApiRequest);
+    const postCodeData = await postCodeResponse.json();
+    //console.log(postCodeData);
 
+    const latitude=postCodeData['result'].latitude;
+    const longitude=postCodeData['result'].longitude;
+
+    const busRadiusSearchApi='https://api.tfl.gov.uk/StopPoint/?lat='+latitude+'&lon='+longitude+'&stopTypes=NaptanPublicBusCoachTram';
+    const radiusResponse = await fetch(busRadiusSearchApi);
+    const radiusData = await radiusResponse.json();
+    //console.log(radiusData);
+
+    
+    const busStop1=radiusData.stopPoints[0].naptanId;
+    const busStop2=radiusData.stopPoints[1].naptanId;
+    
     
 
 }
 
-getBusStopData("Hello, bus stop code would you like to inspect?");
+//getBusStopData("Hello, bus stop code would you like to inspect?");
+findNearestStops('Enter the post code to be searched for:');
