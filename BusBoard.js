4a1211c0cd0846ea9df9e07d44ee842e11ff1504
Jack Hughes <Jack.Hughes@softwire.com>
2022-08-12 09:21:38 +0100

    post code request API added

--- BusBoard.js
+++ BusBoard.js
@@ -1,32 +1,42 @@
 const readline = require('readline-sync');
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
 
-async function getBusStopData(askForStopCode) {
-    console.log(askForStopCode);
+async function getBusStopData(requestStopCode) {
+    console.log(requestStopCode);
     stopCode = readline.prompt();
     // apiRequest = "https://api.tfl.gov.uk/StopPoint/"+stopCode+"/Arrivals"
     apiRequest = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals"
 
     // fetches API and creates "data" array to hold them in.
-    const response = await fetch(apiRequest);
-    const data = await response.json();
+    const busStopResponse = await fetch(apiRequest);
+    const busStopData = await busStopResponse.json();
 
     // creates empty array for top 5 buses.
     var nextBus = [];
     
     // finds first 5 entries on API and returns certain properties of the buses
     for (i = 0; i < 5; i++) {
-        let busi = {
-            'busName': data[i].lineName,
-            'waitTime': data[i].timeToStation,
-            'destination': data[i].destionationName,
-            'route': data[i].towards
+        let bus = {
+            'busName': busStopData[i].lineName,
+            'waitTime': busStopData[i].timeToStation,
+            'destination': busStopData[i].destionationName,
+            'route': busStopData[i].towards
         };
         
-        nextBus.push(busi);
+        nextBus.push(bus);
     }
     
     console.log(nextBus);
+}
+
+function findNearestStops(requestPostCode) {
+    console.log(requestPostCode);
+    postCode = readline.prompt();
+    // pcApiRequest = "api.postcodes.io/postcodes/"+postCode
+    pcApiRequest = "api.postcodes.io/postcodes/N129HB"
+
+    
+
 }
 
 getBusStopData("Hello, bus stop code would you like to inspect?");
