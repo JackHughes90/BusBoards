39ae27572437f4b738f3b7d1c06fa65ebd8d09c9
Bharathi Ramachandran <Bharathi.Ramachandran@softwire.com>
2022-08-15 14:49:58 +0100

    used error handling

--- BusBoard.js
+++ BusBoard.js
@@ -1,20 +1,32 @@
 const readline = require('readline-sync');
-const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const fetch = (...args) => import('node-fetch').then(({ default: fetch }) => fetch(...args));
 
-async function getBusStopData(stopCode,stopName) {
-    
-    apiRequest = "https://api.tfl.gov.uk/StopPoint/"+stopCode+"/Arrivals"
+async function getBusStopData(stopCode, stopName) {
+
+    apiRequest = `https://api.tfl.gov.uk/StopPoint/${stopCode}/Arrivals`
     //apiRequest = "https://api.tfl.gov.uk/StopPoint/490008660N/Arrivals"
 
     // fetches API and creates "data" array to hold them in.
     const busStopResponse = await fetch(apiRequest);
     const busStopData = await busStopResponse.json();
 
+    
+        try {
+
+            if (busStopData.length === 0)
+                throw "No buses coming!!";
+        }
+        catch (err) {
+            console.log(err);
+        }
+    
+
+
     // creates empty array for top 5 buses.
     var nextBus = [];
 
-    nextBus[0]=stopName;
-    
+    nextBus[0] = stopName;
+
     // finds first 5 entries on API and returns certain properties of the buses
     for (i = 0; i < 5; i++) {
         let bus = {
@@ -23,10 +35,10 @@ async function getBusStopData(stopCode,stopName) {
             'destination': busStopData[i].destionationName,
             'route': busStopData[i].towards
         };
-        
+
         nextBus.push(bus);
     }
-    
+
     console.log(nextBus);
 }
 
@@ -35,43 +47,59 @@ async function findNearestStops(requestPostCode) {
     let postCode;
     let postCodeResponse;
     let postCodeData;
-    
+
     //console.log(postCodeData);
 
-    do
-    {
+    do {
         try {
             postCode = readline.prompt();
-            pcApiRequest = "https://api.postcodes.io/postcodes/"+postCode
+            pcApiRequest = `https://api.postcodes.io/postcodes/${postCode}`;
             // pcApiRequest = "https://api.postcodes.io/postcodes/N129HB"
-            
+
             postCodeResponse = await fetch(pcApiRequest);
             postCodeData = await postCodeResponse.json();
             if (postCodeData.status === 404) throw "Invalid post code";
         }
-        catch(err) {
+        catch (err) {
             console.log(err);
         }
     } while (postCodeData.status === 404);
 
-    const latitude=postCodeData['result'].latitude;
-    const longitude=postCodeData['result'].longitude;
-
-    const busRadiusSearchApi = `https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=500`;
-    const radiusResponse = await fetch(busRadiusSearchApi);
-    const radiusData = await radiusResponse.json();
+    const latitude = postCodeData['result'].latitude;
+    const longitude = postCodeData['result'].longitude;
+    let busRadiusSearchApi;
+    let radiusResponse;
+    let radiusData;
+    let searchRadius;
+    console.log('Enter the radius within which you want to search for the bus stop (in meters):');
     //console.log(radiusData);
 
-    for(let i=0;i<2;i++){
-    const busStop=radiusData.stopPoints[i].naptanId;
-    const busStopName=radiusData.stopPoints[i].commonName;
-    getBusStopData(busStop,busStopName);
+    do {
+        try {
+            searchRadius = readline.prompt();
+            busRadiusSearchApi = `https://api.tfl.gov.uk/StopPoint/?lat=${latitude}&lon=${longitude}&stopTypes=NaptanPublicBusCoachTram&radius=${searchRadius}`;
+            radiusResponse = await fetch(busRadiusSearchApi);
+            radiusData = await radiusResponse.json();
+            if (radiusData.stopPoints.length === 0)
+                throw "No bus stop nearby!Increase the search radius.";
+        }
+        catch (err) {
+            console.log(err);
+        }
+    } while (radiusData.stopPoints.length === 0)
+
+
+    for (let i = 0; i < 2; i++) {
+        const busStop = radiusData.stopPoints[i].naptanId;
+        const busStopName = radiusData.stopPoints[i].commonName;
+        getBusStopData(busStop, busStopName);
     }
 }
 
+
 // a function to guide the user to the nearest bus stop.
 async function journeyPlanner(postCode, busStopCode) {
-    
+
     // request API
     const plannerResponse = await fetch('https://api.tfl.gov.uk/Journey/JourneyResults/n129hb/to/n129hj');
     const plannerData = await plannerResponse.json();
@@ -80,10 +108,10 @@ async function journeyPlanner(postCode, busStopCode) {
 
         // for each step, grab descriptionHeading and description.
         console.log(plannerData.journeys[0].legs[0].instruction.steps[i].descriptionHeading
-             + " " + plannerData.journeys[0].legs[0].instruction.steps[i].description + ";");
-        
+            + " " + plannerData.journeys[0].legs[0].instruction.steps[i].description + ";");
+
         // move onto next step.
-        
+
     }
     // "you have arrived"
     console.log("You have arrived!");
